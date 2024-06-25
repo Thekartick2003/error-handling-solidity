c4b97571aba5de72e444d226e311da54cde699f4
KARTICK <145479917+Thekartick2003@users.noreply.github.com>
2024-06-25 13:36:55 +0530

    Update errorhandling.sol
    my code about the error handling and each function tells about the error handling methods named revert, assert, and require.

--- errorhandling.sol
+++ errorhandling.sol
@@ -30,7 +30,7 @@ function assert_marks(uint _maths, uint _Science, uint _Geography) public {
 
 function get_result() public view returns(string memory){
 //Here If the cumaltive marks is greater than equal to 200 then person is eligible for the certificate/result.
-    if (result >= 200 || result <300){
+    if (result >= 200 && result <300){
         return "you are passed, congratualtion !!!";
     } 
     //If the person cumaltive marks is less than 200 then person is not eligible for the certificate and result, reapper in the exam.
